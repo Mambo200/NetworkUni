9f5f546e5c27aac174b6a841e0a392085946ad72
DESKTOP-BC3V8EV\Tobias <tobis-stroedicke@gmx.de>
2019-03-04 22:08:18 +0100

    Enemy has path to go

---
+++ Assets/Scripts/EnemyWalkPositions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemyWalkPositions : NetworkBehaviour
+{
+    public static Transform[] Get { get; private set; }
+
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Initialize();
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        // get every object
+        Transform[] go = this.GetComponentsInChildren<Transform>();
+
+        List<Transform> list = new List<Transform>();
+        // filter this
+        foreach (Transform t in go)
+        {
+            if (t != this.transform)
+            {
+                list.Add(t);
+            }
+        }
+
+        Get = list.ToArray();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extensions
+{
+    public static class ArrayExtension
+    {
+        private static Random r = new Random();
+
+        public static T GetOne<T>(this T[] _array)
+        {
+            return _array[r.Next(0, _array.Length)];
+        }
+    }
+}
